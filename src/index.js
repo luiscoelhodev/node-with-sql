7a45e707d476bc7ca91c1ce56f850479e52ae1a2
Luis Coelho <mancoeluis@gmail.com>
2022-05-10 23:59:51 -0300

    feat: app config in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// API config to be set here
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+    console.log("Server running on port 3333.")
+})
+
+app.get('/', (req, res) => {
+    return res.status(200).send({message: `Hello world!`})
+})
